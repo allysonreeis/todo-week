47a431cd0a67d62e7d612a5d9576befbe25fe788
Allyson Reis <allysonreis1@gmail.com>
2020-05-18 13:53:31 -0300

    Adicionando to-dos dentre de uma array de objetos

--- scripts.js
+++ scripts.js
@@ -1,30 +1,91 @@
 let todoList = [];
-let todoListEl = document.querySelector('.todoList');
 
-document.querySelector('input').addEventListener('keyup', (event)=>{
+let todoWeekData = [
+  {
+    dayName: "monday",
+    todos: []
+  },
+  {
+    dayName: "tuesday",
+    todos: []
+  },
+  {
+    dayName: "wednesday",
+    todos: []
+  },
+  {
+    dayName: "thursday",
+    todos: []
+  },
+  {
+    dayName: "friday",
+    todos: []
+  },
+  {
+    dayName: "saturday",
+    todos: []
+  },
+  {
+    dayName: "sunday",
+    todos: []
+  }
+]
+
+let inputSelected = document.querySelectorAll('input');
+for (var i = 0; i < inputSelected.length; i++) {
+  inputSelected[i].addEventListener('keyup', enterKey);
+} 
+
+function enterKey (event) {
   if (event.key === 'Enter') {
-    todoList.push(event.target.value);
+    
+    for (var todoDay of todoWeekData) {
+      if (todoDay.dayName == event.target.parentNode.id) {
+        todoDay.todos.push(event.target.value);
+      }
+    }
     event.target.value = '';
     renderTodo();
   }
-  
-});
+}
 
-document.querySelector('.add_button').addEventListener('click', event => {
-  let inputEl = document.querySelector('input');
-  todoList.push(inputEl.value);
+let buttonSelected = document.querySelectorAll('.add_button');
+for (var i = 0; i < buttonSelected.length; i++) {
+  buttonSelected[i].addEventListener('click', mouseClick);
+} 
+
+function mouseClick (event) {
+  
+  let nameId = event.target.offsetParent.offsetParent.id;
+  let inputEl = document.querySelector(`#${nameId}>input`);
+    for (var todoDay of todoWeekData) {
+      
+      if (todoDay.dayName == nameId) {
+        todoDay.todos.push(inputEl.value);
+      }
+    }
+  
   inputEl.value = '';
   renderTodo();
-});
+}
 
 function renderTodo () {
-  todoListEl.innerHTML = '';
   
-  todoList.forEach((todo)=> {
-    var li = document.createElement('li');
+  for (var todoDay of todoWeekData) {
     
-    li.innerHTML = `<li>${todo}</li>`;
+    let nameDiv = document.querySelector(`#${todoDay.dayName}>ul`);
+    nameDiv.innerHTML = '';
 
-    todoListEl.append(li);
-  });
+    for (var i = 0; i < todoDay.todos.length; i++) {
+      
+      if (todoDay.dayName == nameDiv.parentElement.id) {
+        var li = document.createElement('li');
+        li.innerHTML = `<li>${todoDay.todos[i]}</li>`;
+
+        nameDiv.append(li);
+      }
+
+    }
+    
+  }
 }
